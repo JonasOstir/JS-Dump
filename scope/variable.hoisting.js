a23015d12b1dd34e7e85f93be743346228b31f1e
Jonas Ostir <Jonas.Ostir@gmail.com>
2017-12-04 15:35:07 +0100

    Variable Hoisting

---
+++ scope/variable.hoisting.js
@@ -0,0 +1,76 @@
+'use strict'
+
+console.log(name); // undefined
+var name = 'Jonas';
+
+// ||
+// ||
+// \/
+
+var name;
+console.log(name);
+name = 'Jonas';
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+function foo() {
+  console.log(name); // undefined
+  var name = 'Jonas';
+}
+
+// ||
+// ||
+// \/
+
+foo(); // undefined (console.log(name): 29); From within the function
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+function foo() {
+  var name;
+  console.log(name);
+  name = 'Jonas'
+}
+
+// ||
+// ||
+// \/
+
+function foo() { // Function gets hoisted to the top of the file.
+  var name;
+  console.log(name);
+  name = 'Jonas'
+}
+
+foo();
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+foo(); // TypeError: foo is not a function
+
+var foo = function() {
+  var name;
+  console.log(name);
+  name = 'Jonas'
+}
+
+// ||
+// ||
+// \/
+
+var foo;
+foo(); // foo isn't defined yet and no function
+
+foo = function() {
+  var name;
+  console.log(name);
+  name = 'Jonas'
+}
+
+foo();
